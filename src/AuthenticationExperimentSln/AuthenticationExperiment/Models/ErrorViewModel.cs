550b71c8246378d450740914ad2b665653d35913
mridulhossain <mridulcse8@gmail.com>
2026-04-04 11:17:58 +0600

    initial project

---
+++ src/AuthenticationExperimentSln/AuthenticationExperiment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AuthenticationExperiment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
